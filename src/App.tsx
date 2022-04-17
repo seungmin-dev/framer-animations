4acf509185258447a750a7a240de0a98a46b1ff2
seungmin-dev <seungmin.dev@gmail.com>
2022-04-18 00:46:30 +0900

    variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,18 +10,23 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+};
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box variants={myVars} initial="start" animate="end" />
     </Wrapper>
   );
 }
