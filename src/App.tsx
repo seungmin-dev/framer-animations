88d2cecd112322203f420cf50f904c4a811d5ec0
seungmin-dev <seungmin.dev@gmail.com>
2022-04-18 20:25:19 +0900

    code challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import styled from "styled-components";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -8,63 +8,123 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
+  grid-template-columns: repeat(2, 1fr);
   gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
 `;
 
 const Box = styled(motion.div)`
-  height: 100px;
-  background-color: rgba(255, 255, 255, 1);
+  width: 250px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.5);
   border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  font-size: 26px;
+`;
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
+  background-color: white;
+`;
+
+const Btn = styled(motion.button)`
+  border: none;
+  background-color: white;
+  padding: 10px;
+  border-radius: 7px;
+  font-weight: 600;
+  font-size: 18px;
 `;
 
 const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-function App() {
+const overlayVar = {
+  initial: { backgroundColor: "rgba(0,0,0,0)" },
+  animate: { backgroundColor: "rgba(0,0,0,0.5)" },
+  exit: { backgroundColor: "rgba(0,0,0,0)" },
+};
+
+export default function App() {
   const [id, setId] = useState<null | string>(null);
+  const [clicked, setClicked] = useState(false);
+  const toggleClick = () => {
+    setClicked((prev) => !prev);
+  };
+
   return (
     <Wrapper>
       <Grid>
-        {[1, 2, 3, 4].map((i) => (
-          <Box onClick={() => setId(i + "")} key={i} layoutId={i + ""}></Box>
-        ))}
+        <Box
+          whileHover={{ scale: 1.05 }}
+          onClick={() => setId("1")}
+          key={"1"}
+          layoutId={"1"}
+          style={{ transformOrigin: "100% 100%" }}
+        />
+        <Box
+          whileHover={{ scale: 1.05 }}
+          onClick={() => setId("2")}
+          key={"2"}
+          layoutId={"2"}
+          style={{ transformOrigin: "0% 100%" }}
+        >
+          {!clicked ? <Circle layoutId="circle" /> : null}
+        </Box>
+        <Box
+          whileHover={{ scale: 1.05 }}
+          onClick={() => setId("3")}
+          key={"3"}
+          layoutId={"3"}
+          style={{ transformOrigin: "100% 0%" }}
+        >
+          {clicked ? <Circle layoutId="circle" /> : null}
+        </Box>
+        <Box
+          whileHover={{ scale: 1.05 }}
+          onClick={() => setId("4")}
+          key={"4"}
+          layoutId={"4"}
+          style={{ transformOrigin: "0% 0%" }}
+        />
       </Grid>
+      <Btn onClick={toggleClick}>Switch</Btn>
       <AnimatePresence>
         {id ? (
           <Overlay
             onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+            variants={overlayVar}
+            initial="initial"
+            animate="animate"
+            exit="exit"
           >
-            <Box layoutId={id} style={{ width: 200, height: 100 }} />
+            <Box
+              key={id}
+              layoutId={id}
+              style={{
+                width: "250px",
+                height: "200px",
+                backgroundColor: "white",
+              }}
+            />
           </Overlay>
         ) : null}
       </AnimatePresence>
     </Wrapper>
   );
 }
-
-export default App;
